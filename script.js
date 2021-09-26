58bc5d10fc568995ce78962f503394c980571690
Zahi Farah <zahi.farah@protonmail.com>
2021-09-26 23:24:35 +0300

    Define populateUI() which gets items from local storage and adds the "selected" class where appropriate. Also, call updateSelectedCount() on page refresh.

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 let ticketPrice = parseInt(movieSelect.value);
 
+populateUI();
+
 // Save selected movie index + price (in local storage)
 function setMovieData(movieIndex, moviePrice) {
   localStorage.setItem("selectedMovieIndex", movieIndex);
@@ -15,12 +17,29 @@ function setMovieData(movieIndex, moviePrice) {
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected"); // NodeList
   const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
-
   localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
-
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Get data from local storage and populate UI
+function populateUI() {
+  // pull out selected seats from local storage + turn it into array from string
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  console.log(selectedSeats);
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
 }
 
 // Movie select event
@@ -40,3 +59,6 @@ container.addEventListener("click", (e) => {
     updateSelectedCount();
   }
 });
+
+// Initial count and total price set
+updateSelectedCount();
